e234814699808672c3ea063ea3889c7485a9723b
EneasSinDiosas <maria.garzo.pulidom@gmail.com>
2023-07-18 10:58:09 +0200

    cssRegister

--- src/components/atoms/Button/Button.component.tsx
+++ src/components/atoms/Button/Button.component.tsx
@@ -10,9 +10,9 @@ interface ButtonProps{
 export const Button: FC<ButtonProps> = ({text, variant = 'filled', children, onClick}) =>{
     let style = 'py-2 px-4 rounded font-semibold transition duration-150 ease-in-out '
     if(variant === 'filled'){
-        style = `${style}  bg-lime-600 shadow-lg hover:bg-green-600 hover:shadow-lg text-indigo-100`
+        style = `${style}  bg-green-600 shadow-lg hover:bg-teal-800 hover:shadow-lg text-indigo-100 hover:scale-90`
     } else {
-        style = `${style} bg-red-500 shadow-md hover:bg-indigo-500 text-indigo-100 border border-indigo-200 hover:border-transparent hover:text-white`
+        style = `${style} bg-red-500 shadow-md hover:bg-red-800 text-teal-100 hover:shadow-inner hover:text-white hover:scale-90`
     }
 
     return (
