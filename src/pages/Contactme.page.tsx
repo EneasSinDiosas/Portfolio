7c10a7870673c8ff7ae7e508f706b5eff9538a43
maria.garzo <maria.garzo.pulidom@gmail.com>
2024-11-14 10:52:44 +0100

    Cambio de linkedIn

--- src/pages/Contactme.page.tsx
+++ src/pages/Contactme.page.tsx
@@ -29,37 +29,37 @@ export const Contactme = () => {
         form.current as HTMLFormElement, 
         'GisKrUbY4VmiqBrEq')
       .then(
-        (result) => {
+        (result: any) => {
           console.log(result.text);
           alert('Mensaje enviado');
       }, 
-        (error) => {
+        (error: any) => {
           console.log(error.text);
       });
   };
-    return(
-        <div className="w-screen h-screen overflow-y-auto items-center justify-center flex flex-col bg-gradient-to-b from-lime-100 to-green-200 dark:bg-gradient-to-b dark:from-emerald-800  dark:to-teal-950">
-           <CardLayout>
-            <h1 className="text-teal-700 font-bold dark:text-green-200">Contact me </h1>
-                   <form ref={form} onSubmit={sendEmail}>
-                   <input type="hidden" name="contact_number"/>
-                   <Input type='text' name='contact-name'>Nombre</Input>
-                    <Input type="email" name="user_email">Correo electrónico</Input>
-                    <Textarea name="message">Escribeme</Textarea>
-                    <div className='flex flex-row w-full mt-3 gap-2 justify-end items-end'>
-                        <Button type='submit'>
-                            <TelegramIcon/>
-                        </Button>
-                        <HomeButton/>
-                    </div>
-                   </form>
+  return(
+    <div className="w-screen h-screen overflow-y-auto items-center justify-center flex flex-col bg-gradient-to-b from-lime-100 to-green-200 dark:bg-gradient-to-b dark:from-emerald-800  dark:to-teal-950">
+      <CardLayout>
+        <h1 className="text-teal-700 font-bold dark:text-green-200">Contact me </h1>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="hidden" name="contact_number"/>
+          <Input type='text' name='contact-name'>Nombre</Input>
+          <Input type="email" name="user_email">Correo electrónico</Input>
+          <Textarea name="message">Escribeme</Textarea>
+          <div className='flex flex-row w-full mt-3 gap-2 justify-end items-end'>
+            <Button type='submit'>
+              <TelegramIcon/>
+            </Button>
+              <HomeButton/>
+          </div>
+        </form>
             
-                <h1 className='text-teal-800 dark:text-green-200'><RssFeedIcon fontSize='large'/>Mis RRSS</h1>
-            <div className='flex flex-row'>
-                <Abutton href="https://www.linkedin.com/in/maria-garzo-6a8831260/" src={colorTheme === 'dark' ? linkedIndark : linkedInLight}/>
-                <Abutton href="https://github.com/EneasSinDiosas" src={colorTheme === 'dark' ? githubDark : githubLight} />
-            </div>
-           </CardLayout>
+        <h1 className='text-teal-800 dark:text-green-200'><RssFeedIcon fontSize='large'/>Mis RRSS</h1>
+        <div className='flex flex-row'>
+          <Abutton href="www.linkedin.com/in/maria-garzo-pulido" src={colorTheme === 'dark' ? linkedIndark : linkedInLight}/>
+          <Abutton href="https://github.com/EneasSinDiosas" src={colorTheme === 'dark' ? githubDark : githubLight}/>
         </div>
-    )
+      </CardLayout>
+    </div>
+  )
 }
